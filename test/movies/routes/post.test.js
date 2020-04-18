a9523e6ca0e42b6fb147eef82336f61116170d37
jakubsaleniuk <jakub.saleniuk@gmail.com>
2020-04-18 18:34:25 +0200

    database

--- test/movies/routes/post.test.js
+++ test/movies/routes/post.test.js
@@ -1,3 +1,6 @@
+process.env.NODE_ENV = 'test'
+process.env.DB_PATH = './test/movies/testdb.json'
+
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../../../index')
